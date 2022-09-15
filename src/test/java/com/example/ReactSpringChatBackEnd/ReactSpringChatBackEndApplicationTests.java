6fc6c0058085a000834e9ae21f7b613eae5d70bb
kymokim <kymokim@gmail.com>
2022-09-15 13:44:53 +0900

    first commit

---
+++ src/test/java/com/example/ReactSpringChatBackEnd/ReactSpringChatBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactSpringChatBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringChatBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
