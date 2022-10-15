376f97159f10e9f8552841d6fbee998ee475a64d
kymokim <kymokim@gmail.com>
2022-10-16 07:25:39 +0900

    test

--- src/main/java/com/example/ReactSpringChatBackEnd/common/config/JwtConfig.java
+++ src/main/java/com/example/ReactSpringChatBackEnd/common/config/JwtConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:/secrets/jwt-secret-key.properties")
+@PropertySource("classpath:/secret/jwt-secret-key.properties")
 @Configuration
 public class JwtConfig {
     @Value("${jwt.secret}") //secret을 환경 변수에서 가져옴
