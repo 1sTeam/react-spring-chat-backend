0005bb6579ba4c19bb1629174a1cf16d8cc2d27e
kymokim <kymokim@gmail.com>
2022-11-09 09:38:47 +0900

    test

--- src/main/java/com/example/ReactSpringChatBackEnd/common/config/WebConfig.java
+++ src/main/java/com/example/ReactSpringChatBackEnd/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.ReactSpringChatBackEnd.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOriginPatterns("*", "http://localhost:3000") //cors 문제 해결
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
 
     }
 }
